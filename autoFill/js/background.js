f5ebdcd51ef36b97b3ce5bf004d7158092489eb0
wanrong_lai@163.com <wanrong_lai@163.com>
2016-12-15 14:31:18 +0800

    chrome插件大体完成，表单查找需要重构

---
+++ autoFill/js/background.js
@@ -0,0 +1,67 @@
+//内置用户名密码url
+var loginMsg = {
+	url 		: null,
+	username 	: null,
+	password 	: null,
+};
+
+//缓冲页面
+var pageSkipSrc =  "/swa/sso/post/";
+
+//用于组织本地刷新登录
+var toLogin = false;
+
+//将用户名密码发送到content中(页面元素属性才能被赋值)
+function sendMsgToLoginContent(tab){
+	chrome.tabs.sendMessage(tab.id, {username: loginMsg.username, password: loginMsg.password}, function(response) {
+
+		if( response !== undefined ){
+			console.log("接收到登录数据:",response);
+			toLogin = false;
+		}else{
+			console.log("接收登录数据中......");
+		}
+
+	});
+}
+
+//从岂同页面中获取到我们需要的用户名密码url数据
+function getLoginMsgFormQt(tab){
+	chrome.tabs.sendMessage(tab.id, {}, function(response) {
+		// console.log("拿到登录数据了吗？",response.url,response.username,response.password);
+		loginMsg.url = response.url;
+		loginMsg.username = response.username;
+		loginMsg.password = response.password;
+		toLogin = true;
+	});
+}
+
+// 加载后动作（1 对的url中显示ico 2对的url中填充表单）
+function checkForValidUrl(tabId, changeInfo, tab) {
+	// console.log("tabId=",tabId,"changeInfo=",changeInfo,"tab=",tab,"\n","==cabbage===",tab.url.toLowerCase().indexOf("http://qq.cabbage.com/")!=-1);
+
+	if( tab.url.toLowerCase().indexOf(pageSkipSrc) != -1 ){
+
+		//在岂同页面中 获取数据填入loginMsg中
+		chrome.pageAction.show(tabId);
+		getLoginMsgFormQt(tab);
+
+	}
+
+	if ( toLogin ) {
+		console.log("现在开始登录，登录信息为：");
+		console.log(loginMsg);
+		if( tab.url.toLowerCase() == loginMsg.url || tab.url.toLowerCase().indexOf(loginMsg.url+"?") != -1 ){
+			//在登录页面中 将数据填入表单中 登录操作
+			chrome.pageAction.show(tabId);
+			sendMsgToLoginContent(tab);
+
+			// window.onload=function(){
+			// }
+		}
+	}
+
+};
+
+// 添加监听
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
---
+++ autoFill/js/content.js
@@ -0,0 +1,235 @@
+//登录次数 连续登录5此不成功则取消登录
+var loginCount = 0;
+
+//接收或发送数据
+chrome.runtime.onMessage.addListener(
+	function(msg, sender, sendResponse) {
+
+		if( msg.username !== undefined ){
+			//拿到username password数据并填写提交表单
+			sendResponse("yes");
+			onFillForm(msg.username, msg.password);
+		}else{
+			//获取岂同缓冲页码的username password url并返回到插件background.js中
+			sendResponse(getLoginValue());
+		}
+	}
+);
+
+// 获取岂同缓冲页面的数据
+function getLoginValue(){
+
+	var loginMsg = {
+		url: "",
+		username: "",
+		password: "",
+	}
+
+	var urlElement = document.getElementById("qt_url_input");
+	var usernameElement = document.getElementById("qt_username_input");
+	var pwdElement = document.getElementById("qt_password_input");
+
+	loginMsg.url = urlElement.getAttribute("value").replace(/^\s+|\s+$/g,"");
+	loginMsg.username = usernameElement.getAttribute("value").replace(/^\s+|\s+$/g,"");
+	loginMsg.password = pwdElement.getAttribute("value").replace(/^\s+|\s+$/g,"");
+
+	// console.log("======get url:",loginMsg.url,loginMsg.url.length);
+	// console.log("======get username:",loginMsg.username,loginMsg.username.length);
+	// console.log("======get password:",loginMsg.password,loginMsg.password.length);
+
+	return loginMsg;
+}
+
+// todo 为空时的判断以及处理
+//  input经常无法准确匹配
+
+//选择用户名输入框
+function selectUsernameInput(formElement){
+
+	var formChildElement = formElement;
+	var inputList = null;
+	var uNameInput = null;
+
+	if( formChildElement != null ){
+		inputList = formChildElement.getElementsByTagName("input");
+	}else{
+		inputList = document.getElementsByTagName("input");
+	}
+
+	for (var i = 0; i < inputList.length; i++) {
+
+		if ( inputList[i].getAttribute("type") == "text" ){
+
+			if(	inputList[i].getAttribute("name").toLowerCase() .indexOf("username") != -1 
+				|| inputList[i].getAttribute("name").toLowerCase() .indexOf("account") != -1 
+				|| inputList[i].getAttribute("name").toLowerCase() .indexOf("email") != -1){
+
+				uNameInput =  inputList[i];
+				break;
+
+			}else if( i == inputList.length-1 ){
+
+				uNameInput =  inputList[0];
+
+			}
+
+		}
+
+	}
+
+	return uNameInput;
+}
+
+//选择密码输入框
+function selectPwdInput(){
+
+	var inputList = document.getElementsByTagName("input");
+	var pwdInput = null;
+
+	for (var i = 0; i < inputList.length; i++) {
+
+		if ( inputList[i].getAttribute("type") == "password" ){
+			pwdInput= inputList[i];
+			break;
+		}
+	}
+
+	return pwdInput;
+}
+
+//选择登录按钮
+function selectLoginBtn(formElement){
+
+	var formChildElement = formElement;
+
+	var inputList = null;
+	var btnList = null;
+	var submitBtn=null;
+
+	// 循环所有btn/ input查询type="submit"的btn
+	function selectSubmitBtnFromList(arr){
+		var subBtn = null;
+
+		for (var i = 0; i < arr.length; i++) {
+
+			if (arr[i].getAttribute("type") == "submit"){
+				subBtn = arr[i];
+				break;
+			}
+		}
+
+		return subBtn;
+	}
+
+	if( formChildElement != null ){
+
+		inputList = formChildElement.getElementsByTagName("input");
+		submitBtn = selectSubmitBtnFromList(inputList);
+
+		if( submitBtn == null ){
+			btnList = formChildElement.getElementsByTagName("button");
+			submitBtn = selectSubmitBtnFromList(btnList);
+		}
+
+	}else{
+
+		inputList = document.getElementsByTagName("input");
+		submitBtn = selectSubmitBtnFromList(inputList);
+
+		if( submitBtn == null ){
+			btnList = document.getElementsByTagName("button");
+			submitBtn = selectSubmitBtnFromList(btnList);
+		}
+
+	}
+
+	// if( submitBtn == null ){
+	// 	submitBtn = document.getElementsByTagName("button")[0];
+	// }
+
+	return submitBtn;
+}
+
+//选择登录Form表单
+function selectLoginForm(pwdElement){
+
+	var formChildElement = pwdElement;
+
+	if( formChildElement != null ){
+
+		do{
+			formChildElement = formChildElement.parentNode;
+			if( formChildElement.tagName.toLowerCase() == "form" ){
+				return formChildElement;
+			}
+		}while( formChildElement.parentNode.tagName.toLowerCase() != "body" );
+
+	}else{
+		
+		var formList = document.getElementsByTagName("form");
+
+		for (var i = 0; i < formList.length; i++) {
+
+			if (formList[i].getAttribute("method").toLowerCase() == "post"){
+				return formList[i];
+			}else if(  i == formList.length-1 ){
+				return formList[0];
+			}
+		}
+		
+	}
+
+}
+
+//填充表单并提交
+function onFillForm(username,password){
+
+	console.log("==username==",username,"==password===",password);
+
+	//得到pwd input
+	var passwordInput = selectPwdInput();
+	console.log("===input pwd=",passwordInput);
+
+	//得到提交form
+	var submitForm = selectLoginForm(passwordInput);
+	console.log("===表单=",submitForm);
+
+	//得到username input
+	var userNameInput = selectUsernameInput(submitForm);
+	console.log("===input username=",userNameInput);
+
+
+	//得到提交btn 或input[type="submit"]
+	var submitButton = selectLoginBtn(submitForm);
+	console.log("===按钮=",submitButton);
+
+	// 给元素填充值
+	if( userNameInput != null ){
+		userNameInput.setAttribute("value", username);
+		passwordInput.value = username;
+	}
+
+	if( passwordInput != null ){
+		passwordInput.setAttribute("value", password);
+	}
+
+	// 触发登录事件
+	if( submitForm != null ){
+
+		if( submitButton != null ){
+			console.log("按钮提交表单");
+			// submitButton.click();
+			return;
+		}
+
+		console.log("表单提交");
+		submitForm.submit();
+
+	}else if( submitButton != null ){
+
+		console.log("按钮点击提交");
+		submitButton.click();	
+
+	}
+
+}
