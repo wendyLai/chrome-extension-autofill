5ec9cad9fcb1ee4c4569f59f6c0fe1b8d0204682
wanrong_lai@163.com <wanrong_lai@163.com>
2016-12-16 15:36:58 +0800

    允许两次登录

--- autoFill/js/background.js
+++ autoFill/js/background.js
@@ -9,15 +9,23 @@ var loginMsg = {
 var pageSkipSrc =  "/swa/sso/post/";
 
 //用于阻止本地刷新登录
+var loginCount = null;
+
+//保存流程状态
 var curStatus = "togetmsg"; // togetmsg → tofillmsg → tologin
 
 //将用户名密码发送到content中(页面元素属性才能被赋值)
 function sendMsgToLoginContent(tab){
+	console.log("发送数据");
 	chrome.tabs.sendMessage(tab.id, {username: loginMsg.username, password: loginMsg.password, url: loginMsg.url}, function(response) {
 
+		if( response !== undefined && curStatus != "tologin" ){
+			loginCount++;
+		}
+
 		if( response !== undefined ){
-			console.log("接收到登录数据:",response);
-			curStatus="tologin"
+			console.log("接收到登录数据,第"+loginCount+"次开始进行填充表单登录操作。");
+			curStatus = "tologin";
 		}else{
 			console.log("接收登录数据中......");
 		}
@@ -28,7 +36,7 @@ function sendMsgToLoginContent(tab){
 //从岂同页面中获取到我们需要的用户名密码url数据
 function getLoginMsgFormQt(tab){
 	chrome.tabs.sendMessage(tab.id, {}, function(response) {
-		console.log("拿到登录数据了吗？",response.url,response.username,response.password);
+		console.log("拿到岂同页面的登录数据:",response.url,response.username,response.password);
 
 		if( response.url != null ){
 			loginMsg.url = response.url;
@@ -42,6 +50,7 @@ function getLoginMsgFormQt(tab){
 
 		if( loginMsg.url != null && loginMsg.username != null && loginMsg.password != null ){
 			curStatus = "tofillmsg";
+			loginCount = 0;//第一次登录
 		}
 	});
 }
@@ -50,8 +59,10 @@ function getLoginMsgFormQt(tab){
 function checkForValidUrl(tabId, changeInfo, tab) {
 	// console.log("tabId=",tabId,"changeInfo=",changeInfo,"tab=",tab,"\n","==cabbage===",tab.url.toLowerCase().indexOf("http://qq.cabbage.com/")!=-1);
 
-	console.log("curStatus=",curStatus);
-	
+	console.log("****************************************************************8");
+	// console.log("开始监听=状态为===",curStatus);
+	// console.log("开始监听=登录次数为===",loginCount);	
+
 	if( tab.url.toLowerCase().indexOf(pageSkipSrc) != -1 ){
 
 		//在岂同页面中 获取数据填入loginMsg中
@@ -60,17 +71,17 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 
 	}
 
+	//允许第二次登录
 	if ( curStatus == "tofillmsg" ) {
-		console.log("现在开始登录，登录信息为：");
-		console.log(loginMsg);
+		console.log("现在发送数据到登录页面，并开始填充表单");
 		if( tab.url.toLowerCase() == loginMsg.url || tab.url.toLowerCase().indexOf(loginMsg.url) != -1 ){
 			//在登录页面中 将数据填入表单中 登录操作
 			chrome.pageAction.show(tabId);
 			sendMsgToLoginContent(tab);
-
-			// window.onload=function(){
-			// }
 		}
+	}else if( loginCount == 1 ){
+		console.log("再次登录");
+		curStatus = "tofillmsg";
 	}
 
 };
